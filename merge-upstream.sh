b4b82c95fff67619def74715d0cba066fb2ca016
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-12-30 00:44:59 +0100

    merge-upstream.sh: simplify error logic

--- merge-upstream.sh
+++ merge-upstream.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
 # https://docs.github.com/en/rest/branches/branches?apiVersion=2022-11-28#sync-a-fork-branch-with-the-upstream-repository
-if response=$(curl --location --fail \
+response=$(curl --location --fail \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $INPUT_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/$INPUT_REPO/merge-upstream" \
-  -d "{\"branch\":\"$INPUT_BRANCH\"}"); then
-  readonly err_code=0
-else
-  readonly err_code=1
-fi
+  -d "{\"branch\":\"$INPUT_BRANCH\"}")
 
+readonly err_code="$?"
 readonly response
 echo "$response" | jq
 
