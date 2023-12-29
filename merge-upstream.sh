a83bb83480282a5575c601ec98438d7c92bc2ec7
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-12-29 23:11:05 +0100

    merge-upstream.sh: improve error handling and debugging

--- merge-upstream.sh
+++ merge-upstream.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
 # https://docs.github.com/en/rest/branches/branches?apiVersion=2022-11-28#sync-a-fork-branch-with-the-upstream-repository
-response=$(curl --location --fail \
+if response=$(curl --location --fail \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $INPUT_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/$INPUT_REPO/merge-upstream" \
-  -d "{\"branch\":\"$INPUT_BRANCH\"}")
+  -d "{\"branch\":\"$INPUT_BRANCH\"}"); then
+  readonly err_code=0
+else
+  readonly err_code=1
+fi
+
+readonly response
+echo "$response" | jq
 
 {
   jq -r '"message=\(.message)"' <<< "$response"
   jq -r '"merge-type=\(.merge_type)"' <<< "$response"
   jq -r '"base-branch=\(.base_branch)"' <<< "$response"
 } >> "$GITHUB_OUTPUT"
+
+exit "$err_code"
