972e0274258d20c5b18d66106f76979cd484f819
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-01-07 13:12:23 +0000

    Mock curl in tests.sh

--- tests.sh
+++ tests.sh
@@ -1,31 +1,42 @@
 #!/usr/bin/env bash
 
 setUp() {
-  assertNotEquals "GITHUB_TOKEN needs to be set" "" "$GITHUB_TOKEN"
+  tmpdir="$(mktemp -d)"
+
+  # Needs jq in PATH
+  export PATH="$PWD/mocks:$PATH"
+  export INPUT_BRANCH="master"
+  export INPUT_REPO="thiagokokada/nixpkgs"
+  export INPUT_TOKEN="$RANDOM"
+  export GITHUB_OUTPUT="$tmpdir/output"
+}
+
+tearDown() {
+  rm -rf "$GITHUB_OUTPUT"
+
+  unset MOCKED_CURL_STDOUT
+  unset MOCKED_CURL_EXIT_CODE
 }
 
 testSuccess() {
-  tmpdir="$(mktemp -d)"
-  export INPUT_BRANCH="${TEST_BRANCH:-master}"
-  export INPUT_REPO="${TEST_REPO:-thiagokokada/nixpkgs}"
-  export INPUT_TOKEN="$GITHUB_TOKEN"
-  export GITHUB_OUTPUT="$tmpdir/output"
+  export MOCKED_CURL_STDOUT='{
+  "message": "Successfully fetched and fast-forwarded from upstream NixOS:master.",
+  "merge_type": "fast-forward",
+  "base_branch": "NixOS:master"
+}'
 
   ./merge-upstream.sh
   assertEquals 0 "$?"
 
-  assertTrue "cat $GITHUB_OUTPUT | fgrep message"
-  assertTrue "cat $GITHUB_OUTPUT | fgrep merge-type"
-  assertTrue "cat $GITHUB_OUTPUT | fgrep base-branch"
+  assertTrue "cat $GITHUB_OUTPUT | fgrep 'message=Successfully fetched and fast-forwarded from upstream NixOS:master.'"
+  assertTrue "cat $GITHUB_OUTPUT | fgrep 'merge-type=fast-forward'"
+  assertTrue "cat $GITHUB_OUTPUT | fgrep 'base-branch=NixOS:master'"
 }
 
 testFail() {
-  export INPUT_BRANCH="${TEST_BRANCH:-master}"
-  export INPUT_REPO="${TEST_REPO:-thiagokokada/nixpkgs}"
-  export INPUT_TOKEN="$RANDOM"
+  export MOCKED_CURL_EXIT_CODE=1
 
   ./merge-upstream.sh
-
   assertNotEquals 0 "$?"
 }
 
